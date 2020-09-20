fe2584b87216755a88d35884f416f4e0b4705572
stijnIsStoer <59971296+stijnIsStoer@users.noreply.github.com>
2020-09-20 12:13:38 +0200

    Update BladSteenSchaar.js

--- commands/BladSteenSchaar.js
+++ commands/BladSteenSchaar.js
@@ -65,6 +65,6 @@ module.exports.run = async (client, message, args) => {
 
 module.exports.help = {
     name: "sps",
-    description: "",
-    category: ""
+    description: "speel steen papier schaar",
+    category: "algemeen"
 }
